c539a17ddd6e645c279b4d598f1c1104a227aac7
ngliangming <69562689+ngliangming@users.noreply.github.com>
2021-03-16 11:42:02 +0800

    Add files via upload

---
+++ src/programme/ms/assignment/ProgrammeMSAssignment.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programme.ms.assignment;
+
+/**
+ *
+ * @author Ng Liang Ming
+ */
+public class ProgrammeMSAssignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
