d7e00c0eca27d6c82a505d0ab0d87fe617112dd1
Ng Liang Ming <ngliangming@gmail.com>
2021-03-16 12:13:39 +0800

    Test Merge NLM

--- src/programme/ms/assignment/ProgrammeMSAssignment.java
+++ src/programme/ms/assignment/ProgrammeMSAssignment.java
@@ -3,7 +3,7 @@ package programme.ms.assignment;
 public class ProgrammeMSAssignment {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Liang was here
     }
     
 }
